1e18bbb9afc2b194b0975c799597b7de9cde8751
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2025-11-07 12:22:21 +0800

    chore: update

---
+++ check.js
@@ -0,0 +1,18 @@
+
+import { exec } from 'child_process';
+import { writeFileSync } from 'fs';
+
+exec('shopify theme check -o json', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${JSON.stringify({error, stdout, stderr}, null ,2)}`);
+        writeFileSync('check.json', stdout);
+        return;
+    }
+    if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        writeFileSync('check.txt', `Stderr: ${stderr}\n${stdout}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    writeFileSync('check.txt', stdout);
+});
