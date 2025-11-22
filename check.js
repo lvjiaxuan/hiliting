9c81a67e7dd81327f45cacd2498d95c218889a9f
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2025-11-22 16:25:38 +0800

    feat: 检查输出

--- check.js
+++ check.js
@@ -4,7 +4,7 @@ import { writeFileSync } from 'fs';
 
 exec('shopify theme check -o json', (error, stdout, stderr) => {
     if (error) {
-        console.error(`exec error: ${JSON.stringify({error, stdout, stderr}, null ,2)}`);
+        console.error(`exec error: ${JSON.stringify({ error, stdout, stderr }, null, 2)}`);
         writeFileSync('check.json', stdout);
         return;
     }
